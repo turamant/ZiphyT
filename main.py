cfd9bd3c21425e5007543f1c8d088dccb91142ba
askvart <tur1amant@gmail.com>
2022-07-29 17:35:20 +0300

    refactoring

--- main.py
+++ main.py
@@ -13,19 +13,32 @@ source = [
     ('c', 'c1'),
 ]
 
-nodes = []
+expected = {
+    'a': {'a1': {}, 'a2': {'a21': {}, 'a22': {}}},
+    'b': {'b1': {'b11': {'b111': {}}}, 'b2': {}},
+    'c': {'c1': {}},
+}
 
 
-def create_tree(in_list, parent):
+def create_tree(in_list: list):
     """рекурсивная функция строит дерево (tree) по списку (in_list) пар id
      (id родителя (parent), id потомка (child))"""
     tree = {}
-    for element in in_list.copy():
-        if element[0] == parent:
-            nodes.append(element[1])
-            tree[element[1]] = create_tree(in_list, element[1])
+    node = tree
+    for i in range(0, len(in_list)):
+        parent = in_list[i][0]
+        child = in_list[i][1]
+        if parent is None:
+            tree.update({child: {}})
+        else:
+            node[parent].update({child: {}})
+            if i != len(in_list) - 1 and in_list[i+1][0] in tree:
+                node = tree
+            elif i != len(in_list) - 1 and in_list[i+1][0] != parent:
+                node = node[parent]
     return tree
 
 
 if __name__ == '__main__':
-    print(create_tree(source, None))
+    assert create_tree(source) == expected
+    print(create_tree(source))
